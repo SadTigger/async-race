fd20de8f0a8ad414ef40d041b0a7d93385f0e44c
Pavel Zhachko <rammiel@yandex.ru>
2021-06-11 17:20:25 +0300

    init: async-race task; feat: add webpack + typescript + eslint to project

---
+++ async-race/src/app.ts
@@ -0,0 +1,9 @@
+export class App {
+  constructor(private readonly rootElement: HTMLElement) {
+    // TODO
+    // this.garage = new Garage();
+    // this.winners = new Winners();
+    // this.rootElement.appendChild(this.garage.element);
+    // this.rootElement.appendChild(this.winners.element);
+  }
+}
---
+++ async-race/src/index.ts
@@ -0,0 +1,14 @@
+import './styles.css';
+
+import { App } from './app';
+
+window.onload = () => {
+  const appElement = document.getElementById('app');
+
+  if (!appElement) {
+    throw new Error('App root element not found');
+  } else {
+    const app = new App(appElement);
+    // app.run();
+  }
+};
