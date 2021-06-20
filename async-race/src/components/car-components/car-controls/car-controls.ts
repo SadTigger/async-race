1668cce747c8fb6c9255ff1a89cbffda9ef457fc
Pavel Zhachko <rammiel@yandex.ru>
2021-06-20 11:47:50 +0300

    fix: car controls buttons active/unactive state

--- async-race/src/components/car-components/car-controls/car-controls.ts
+++ async-race/src/components/car-components/car-controls/car-controls.ts
@@ -4,30 +4,44 @@ import { EngineButton } from '../../race-components/engine-button/engine-button'
 import './car-controls.css';
 
 export class CarControls extends BaseComponent {
-  private startEngineButton?: EngineButton;
+  private startEngineButton: EngineButton;
 
-  private stopEngineButton?: EngineButton;
+  private stopEngineButton: EngineButton;
+
+  private audio: HTMLAudioElement;
 
   constructor() {
     super('div', ['car-controls']);
+    this.startEngineButton = new EngineButton('A');
+    this.stopEngineButton = new EngineButton('B');
+    this.audio = new Audio('./start-engine.wav');
   }
 
   addTemplate(id: string): void {
-    this.startEngineButton = new EngineButton('A');
-    this.startEngineButton.element.addEventListener('click', async () => {
+    this.startEngineButton.element.addEventListener('click', async (event) => {
       // console.log(`start engine for race-${id}`);
-      const audio = new Audio('./start-engine.wav');
       await stopEngine(id);
       const start = await startEngine(id);
       if (start) {
-        audio.play();
+        this.audio.play();
+        if (event.target) {
+          const startButton = event.target as HTMLElement;
+          startButton.classList.remove('active');
+          this.stopEngineButton.element.classList.add('active');
+        }
       }
       // console.log('distance', start.distance);
       // console.log('velocity', start.velocity);
     });
-    this.stopEngineButton = new EngineButton('B');
-    this.stopEngineButton.element.addEventListener('click', async () => {
+    this.stopEngineButton.element.addEventListener('click', async (event) => {
       // console.log(`stop engine for race-${id}`);
+      if (event.target) {
+        const stopButton = event.target as HTMLElement;
+        this.audio.pause();
+        this.audio.currentTime = 0;
+        stopButton.classList.remove('active');
+        this.startEngineButton.element.classList.add('active');
+      }
       await stopEngine(id);
     });
     this.startEngineButton.setActiveState();
